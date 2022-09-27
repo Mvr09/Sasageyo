afd58fae8154063bd8bcb0da9efb1d968d1aa148
Michael V <michaelviertel09@gmail.com>
2022-09-27 18:51:30 -0500

    -M "Enumerator added"

--- Semana 9/PaniniCollector/src/ui/PaniniManager.java
+++ Semana 9/PaniniCollector/src/ui/PaniniManager.java
@@ -130,7 +130,7 @@ public class PaniniManager {
 			reader.nextLine();
 			System.out.println(
 					"Type the position of the player");
-			String playerPosition = reader.nextLine();
+			int playerPosition = reader.nextInt();
 
 			if (controller.registerPlayer(teamID, name, lastName, id, shirtNumber, playerPosition)) {
 				System.out.println("Player was successfully registered");
